8b3a9bc464c4848392c0ec3f4302b8f5d9c27916
Frederick Lazo <fredlazo@yahoo.com>
2016-08-18 14:48:10 -0700

    Checkpoint 4 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818210223) do
+ActiveRecord::Schema.define(version: 20160818211052) do
+
+  create_table "apps", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "apps", ["user_id"], name: "index_apps_on_user_id"
 
   create_table "reg_apps", force: :cascade do |t|
     t.string   "name"
