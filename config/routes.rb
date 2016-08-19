3bfe82a49ccbd7ffa4935a9587133803ed1f6a30
Frederick Lazo <fredlazo@yahoo.com>
2016-08-18 17:56:56 -0700

    Checkpoint-6 completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   resources :apps
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   root 'welcome#index'
 end
